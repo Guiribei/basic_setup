fc83ffc39e9b529965774602fdba9c36833da7ca
Guiribei <guilherme_f_ribeiro@hotmail.com>
2023-05-25 23:56:17 -0300

    feat(DB): add form to sqlite

--- products/models.py
+++ products/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Suggestion(models.Model):
+	text = models.CharField(max_length=400)
+
+	def __str__(self) -> str:
+		return self.text
--- products/views.py
+++ products/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Suggestion
 
 def check_product(request):
-	status = 'online'
-	return render(request, 'check_product.html', {'status':status})
-
+	if request.method == 'GET':
+		status = 'online'
+		return render(request, 'check_product.html', {'status':status})
+	elif request.method == 'POST':
+		text = request.POST.get('suggestion')
+		suggestion = Suggestion(text=text)
+		suggestion.save()
+		return render(request, 'check_product.html')
 def insert_product(request):
 	return HttpResponse('Nice!')
